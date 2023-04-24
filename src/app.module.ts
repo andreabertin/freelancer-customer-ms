f1fdacd1665c18f0aa62ff0799cb20950699d8a9
Andrea Bertin <abertin@andreabertin.net>
2023-04-24 16:23:54 +0200

    Added prom-client for prometheus metrics (default)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CustomerResolver } from '@resolvers/customer.resolver';
 import { CustomerService } from '@services/customer.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import appConfig from '@configs/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [appConfig] }),
+    PrometheusModule.register(), // setups default metrics under GET /metrics path
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver, // uses Apollo
       autoSchemaFile: true, // schema generated in memory
